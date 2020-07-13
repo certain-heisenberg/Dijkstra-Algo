9e85b416963c2822946cc68463b8b174145ec7cb
kineticdud <32921778+kineticdud@users.noreply.github.com>
2020-07-14 02:22:05 +0530

    Add files via upload

---
+++ dijkstra.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+typedef pair<int,int> pi;
+using namespace std;
+
+vector<pi> arr[1001];
+vector<int> dist(1001, INT_MAX);
+
+struct comp{
+    bool operator()(pi &a, pi &b){
+        return a.first<b.first;
+    }
+};
+
+void dijkstra(int v, int d){
+    priority_queue<pi, vector<pi>, comp> pq;
+    pq.push({d, v});
+    
+    while(!pq.empty()){
+        int node=pq.top().second;
+        dist[node]=pq.top().first;
+        pq.pop();
+        
+        for(pi c: arr[node]){
+            int child=c.first;
+            int w=c.second;
+            int len=dist[node]+w;
+            if(len<dist[child]){
+                pq.push({len, child});
+            }
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    cin>>n>>m;
+    for(int i=1; i<=m; i++){
+        int a, b, w;
+        cin>>a>>b>>w;
+        arr[a].push_back({b,w});
+        arr[b].push_back({a,w});
+    }
+    dijkstra(1, 0);
+    for(int i=1; i<=n; i++){
+        cout<<dist[i]<<" ";
+    }
+}
